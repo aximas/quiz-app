31fa509e6727176d3af66cc1776c809c1295b9e2
aximas <northdakota778@gmail.com>
2021-03-17 01:51:28 +0500

    all finished

--- script.js
+++ script.js
@@ -42,8 +42,32 @@ const Data = [
             
 
         ]
+    },
+    {
+        question: 'Вопрос 3',
+        answers: [
+            {
+                id: '47',
+                value: 'Ответ 31',
+                correct: false,
+            },
+            {
+                id: '8',
+                value: 'Ответ 42',
+                correct: true,
+            },
+            {
+                id: '9',
+                value: 'Ответ 53',
+                correct: false,
+            },
+            
+
+        ]
     }
 ];
+
+let localResults = {};
 
 const quiz = document.getElementById('quiz'),
     quizQuestions = document.getElementById('quiz-questions'),
@@ -53,18 +77,19 @@ const quiz = document.getElementById('quiz'),
     btnRestart = document.getElementById('btn-restart');
 
 const renderQuestions = (index) => {
-    const renderAnswers = () => {
-        return Data[index].answers.map((answer) => {
-            return `
-                <li>
+    renderIndicator(index + 1);
+    quizQuestions.dataset.currentStep = index;
+    const renderAnswers = () => Data[index].answers
+          .map((answer) => 
+             `<li>
                     <label>
-                        <input type="radio" name="q1">
+                        <input type="radio" name="${index}" value="${answer.id}" class="answer-input">
                         ${answer.value}
                     </label>
                 </li>
-            `;
-        })
-    }
+            `)
+            .join('');
+    
 
     quizQuestions.innerHTML =`
         <div class="quiz-questions-item">
@@ -73,40 +98,86 @@ const renderQuestions = (index) => {
             </div>
             
             <ul class="quiz-questions-item__answer">
-                <li>
-                    <label>
-                        <input type="radio" name="q1">
-                        Answer 1
-                    </label>
-                </li>
-                <li>
-                    <label>
-                        <input type="radio" name="q1">
-                        Answer 2
-                    </label>
-                </li>
+                ${renderAnswers()}
             </ul>
         </div>
     `;
 }
 
 const renderResults = () => {
+    let content = '';
 
+    const getClassName = (answer, questionIndex) => {
+        let classname = '';
+        if (!answer.correct && answer.id === localResults[questionIndex]) {
+            classname = 'answer--invalid';
+        } else if (answer.correct) {
+            classname = 'answer--valid';
+        }
+
+        return classname;
+    }
+
+    const getAnswers = (questionIndex) =>  Data[questionIndex].answers
+    .map((answer) =>  ` <li class="${getClassName(answer, questionIndex)}">  ${answer.value} </li>`)
+        .join('');
+    
+
+    Data.forEach((question, index) => {
+        content += `
+        <div class="quiz-results-item__question">
+           ${question.question}
+        </div>
+        <ul class="quiz-results-item__answer">${getAnswers(index)}</ul>
+        `;
+    });
+
+    quizResults.innerHTML = content;
 }
 
-const renderIndicator = () => {
+const renderIndicator = (currentStep) => {
+    quizIndicator.innerHTML = `${currentStep}/${Data.length}`;
 
-}
+};
 
 quiz.addEventListener('change', (event) => {
-    // logic of answer
-});
+    if (event.target.classList.contains('answer-input')) {
+        console.log('inputed'); 
+        localResults[event.target.name] = event.target.value;
+        btnNext.disabled = false;
+
+        console.log(localResults);
+       }
+    });
 
 quiz.addEventListener('click', (event) => {
    if (event.target.classList.contains('btn-next')) {
-    console.log('nextBTN'); 
+    const nextQuestionIndex = Number(quizQuestions.dataset.currentStep) + 1;
+    
+    if (Data.length === nextQuestionIndex) {
+        quizQuestions.classList.add('questions--hidden')
+        quizIndicator.classList.add('incator--hidden')
+        quizResults.classList.add('results--visible')
+        btnNext.classList.add('btn-next--hidden')
+        btnRestart.classList.add('btn-restart--visible')
+        renderResults();
+    } else { 
+        renderQuestions(nextQuestionIndex);
+    }
+
+    btnNext.disabled = true;
    }
    if (event.target.classList.contains('btn-restart')) {
-        console.log('restartBTN'); 
+         quizResults.innerHTML = '';
+         let localResults = {};
+        quizQuestions.classList.remove('questions--hidden');
+        quizIndicator.classList.remove('incator--hidden');
+        quizResults.classList.remove('results--visible');
+        btnNext.classList.remove('btn-next--hidden');
+        btnRestart.classList.remove('btn-restart--visible');
+        renderQuestions(0);
+
    }
 });
+
+renderQuestions(0);
